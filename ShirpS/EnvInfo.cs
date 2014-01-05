2b81a7cf44d4e935d9a58e9d57abcaaae64b381e
Rick Burgstaler <rburgstaler@yahoo.com>
2014-01-05 13:08:55 -0600

    - Added some logic for dealing with parsing an Author/Email file to setup foundation for writing a committer author validation script. - Started moving functions used to know what is available (environmental variables and command line variables) to their own source code file.

---
+++ ShirpS/EnvInfo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace ShirpS
+{
+    //Basic utility methods that can be used for debugging (to know what is available)
+    public class EnvInfo
+    {
+        //Output the command line arguments passed in
+        public static void OutputArguments()
+        {
+            String[] args = Environment.GetCommandLineArgs();
+            for (int argidx = 0; argidx < args.Length; argidx++)
+            {
+                Console.Write(String.Format("Argument[{0}]: {1}\n", argidx, args[argidx]));
+            }
+        }
+
+        //Output environmental variables to know what is available
+        public static void OutputEnvironmentalVariables()
+        {
+            IDictionary environmentVariables = Environment.GetEnvironmentVariables();
+            foreach (DictionaryEntry de in environmentVariables) Console.Write("{0} = {1}\n", de.Key, de.Value);
+
+        }
+    }
+}
--- ShirpS/Program.cs
+++ ShirpS/Program.cs
@@ -1,22 +1,56 @@
 ﻿using System;
 using System.Collections;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
 
 namespace ShirpS
 {
+
+    public class ListLookup
+    {
+        public List<String> Names = new List<String>();
+        public List<String> Emails = new List<String>();
+        public void LoadFromFile(String filePath)
+        {
+            Regex reg = new Regex(@"^(.*) <(.*@.*)>$", RegexOptions.IgnoreCase);
+            String[] fl = File.ReadAllLines(filePath);
+            foreach (String ln in fl)
+            {
+                Match m = reg.Match(ln);
+                if (!m.Success) continue;
+                Names.Add(m.Groups[1].Value);
+                Emails.Add(m.Groups[2].Value);
+            }
+            Names.Sort(Comp);
+            Emails.Sort(Comp);
+        }
+        StringComparer Comp = StringComparer.CurrentCultureIgnoreCase;
+        public int FindNameIndex(String name)
+        {
+            return Names.BinarySearch(name, Comp);
+        }
+        public int FindEmailIndex(String email)
+        {
+            return Emails.BinarySearch(email, Comp);
+        }
+    }
 
     class Program
     {
         static void Main(string[] args)
         {
-            //Console.OutputEncoding = Encoding.ASCII;
+            String refname = args[1];
+            String oldrev = args[2];
+            String newrev = args[3];
+
+            ListLookup lookup = new ListLookup();
+            lookup.LoadFromFile("ValidUsers.txt");
             
             Console.Write("*****************************************\n");
             Console.Write("*****************************************\n");
+            Console.Write("Validating your permissions\n");
 
-
-            String refname = args[1];
-            String oldrev  = args[2];
-            String newrev  = args[3];
             //String user    = ENV['USER'];
 
             String[] res = Command.Execute(String.Format("git rev-list {0}..{1}", oldrev, newrev));
@@ -37,14 +71,17 @@ namespace ShirpS
 
 
             Console.Write(Environment.CurrentDirectory + "\n");
-            Console.Write(String.Join("\n", args));
 
             Console.Write("\n****\n");
-            
-            IDictionary	environmentVariables = Environment.GetEnvironmentVariables();
-            foreach (DictionaryEntry de in environmentVariables) Console.Write("{0} = {1}\n", de.Key, de.Value);
 
-            Console.Write("Validating your permissions\n");
+            EnvInfo.OutputArguments();
+
+
+
+            EnvInfo.OutputEnvironmentalVariables();
+            
+
+
             Console.Write("*****************************************\n");
             Console.Write("*****************************************\n");
             Environment.Exit(0);
