6f4a855965b9989931972d1ad24a8571b894ec78
Rick Burgstaler <rburgstaler@yahoo.com>
2015-03-20 22:48:04 -0500

    First iteration of moving to a scripted solution.
    For now the part that calls the script is commented out.

---
+++ SampleScripts/Sample.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace SSRScript
+{
+    public class SSR
+    {
+        public String Exec()
+        {
+             return "Hello world";
+        }
+    }
+}
--- ShirpS/Program.cs
+++ ShirpS/Program.cs
@@ -4,6 +4,10 @@ using System.IO;
 using System.Text.RegularExpressions;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.CSharp;
+using System.CodeDom.Compiler;
+using System.Text;
+using System.Reflection;
 
 namespace ShirpS
 {
@@ -65,6 +69,40 @@ namespace ShirpS
             //First parameter is the path the the bash
             ctx.ScriptFile = Path.Combine(Environment.CurrentDirectory, args[0].Replace('/', '\\'));
             ctx.Args = args.ToList();
+
+
+
+
+
+            /*
+            ScriptRunner sr = new ScriptRunner();
+            sr.AddReferencedAssembly(typeof(String).GetType());
+            //sr.AddReferencedAssembly(typeof(Console).GetType());
+            sr.Compile(File.ReadAllText(@"D:\Dev\Shirps\SampleScripts\Sample.cs"));
+
+            Console.Write("++++++++++Errors+++++++++++\n");
+
+
+            String[] errs = sr.Errors().Split(new String[1] { Environment.NewLine }, StringSplitOptions.None);
+            foreach (String er in errs)
+            {
+                Console.Write(er+"\n");
+
+            }
+
+            
+
+            Object retVal = sr.Execute("SSRScript.SSR.Exec", null, null);
+
+            Console.Write("++++++++++Test+++++++++++\n");
+            Console.Write(retVal.ToString()+"\n");
+            */
+
+
+
+
+
+
             String refname = args[1];
             String oldrev = args[2];
             String newrev = args[3];
@@ -73,7 +111,7 @@ namespace ShirpS
             lookup.LoadFromFile(@"..\ValidUsers.txt");
 
             List<String> errors = new List<String>();
-            
+
             Console.Write("*****************************************\n");
             Console.Write("*****************************************\n");
             Console.Write("Validating your Committers and Authors\n");
@@ -85,7 +123,7 @@ namespace ShirpS
             else res = Command.Execute(String.Format("git rev-list {0}..{1}", oldrev, newrev));
             Console.Write(String.Format("{0} commits found\n", res.Length));
 
-            for (int x = res.Length-1; x >= 0; x--)
+            for (int x = res.Length - 1; x >= 0; x--)
             {
                 String[] newres = Command.Execute(String.Format("git cat-file commit {0}", res[x]));
                 GitCommitInfo gi = GitCommitInfo.Parse(newres);
@@ -104,4 +142,92 @@ namespace ShirpS
             Environment.Exit(errors.Count);
         }
     }
+
+    public class ScriptRunner
+    {
+        CSharpCodeProvider codeProvider = new CSharpCodeProvider(new Dictionary<String, String>() { 
+            //{ "CompilerVersion", "v3.5" }   //3.5 was causing problems for some reason
+        });
+        CompilerParameters parameters = new CompilerParameters();
+        CompilerResults compResults;
+        public bool Compile(String aScript)
+        {
+            runScript = aScript;
+            //Make sure we generate DLL
+            parameters.GenerateExecutable = false;
+
+            //Make a dll
+            parameters.GenerateInMemory = false;
+
+            parameters.IncludeDebugInformation = true;
+
+            parameters.OutputAssembly = "DaDaDa.dll";
+
+            compResults = codeProvider.CompileAssemblyFromSource(parameters, aScript);
+            return compResults.Errors.Count == 0;
+        }
+
+        //Add the assembly by giving a type defined in the assembly
+        public bool AddReferencedAssembly(Type classType)
+        {
+            bool retVal = !parameters.ReferencedAssemblies.Contains(classType.Assembly.Location);
+            if (retVal) parameters.ReferencedAssemblies.Add(classType.Assembly.Location);
+            return retVal;
+        }
+
+
+        String runScript = "";
+        public String Errors()
+        {
+            StringBuilder sBuild = new StringBuilder();
+            if (compResults.Errors.Count > 0)
+            {
+                foreach (CompilerError CompErr in compResults.Errors)
+                {
+                    sBuild.AppendLine("Error compiling Script:");
+                    sBuild.AppendLine(runScript);
+                    sBuild.AppendLine("Line number " + CompErr.Line);
+                    sBuild.AppendLine("Error Number: " + CompErr.ErrorNumber);
+                    sBuild.AppendLine("Error Text: " + CompErr.ErrorText + "';");
+                    sBuild.AppendLine();
+                    sBuild.AppendLine();
+                }
+            }
+            return sBuild.ToString();
+        }
+
+        public Object Execute(String FullMethodName, PropertyAssigns props, params Object[] args)
+        {
+            Int32 idx = FullMethodName.LastIndexOf(".");
+            return InvokeMethodSlow(compResults.CompiledAssembly, FullMethodName.Substring(0, idx), FullMethodName.Substring(idx + 1), args, props);
+        }
+
+        public static Object InvokeMethodSlow(string AssemblyName, string ClassName, string MethodName, Object[] args)
+        {
+            // load the assemly
+            Assembly assembly = Assembly.LoadFrom(AssemblyName);
+            return InvokeMethodSlow(assembly, ClassName, MethodName, args, null);
+        }
+
+
+        public static Object InvokeMethodSlow(Assembly assembly, string ClassName, string MethodName, Object[] args, PropertyAssigns props)
+        {
+            Type tp = assembly.GetType(ClassName, true);
+            object ClassObject = Activator.CreateInstance(tp);
+            if (props != null)
+            {
+                foreach (KeyValuePair<String, Object> keyVal in props)
+                {
+                    PropertyInfo pi = tp.GetProperty(keyVal.Key);
+                    if (pi != null) pi.SetValue(ClassObject, keyVal.Value, new object[0]);
+                }
+            }
+            return tp.InvokeMember(MethodName, BindingFlags.Default | BindingFlags.InvokeMethod, null, ClassObject, args);
+        }
+    }
+
+    public class PropertyAssigns : Dictionary<String, Object>
+    {
+
+    }    
 }
